0997f97e88acccd90916af06967916ac5d5238db
mike levy <michael.levy@techelevator.com>
2023-01-24 12:15:11 -0500

    M1D6 - final lecture files

---
+++ module-1/06_Intro_to_Objects_Strings/lecture-final/src/main/java/com/techelevator/Objs.java
@@ -0,0 +1,21 @@
+package com.techelevator;
+
+import java.util.Scanner;
+
+public class Objs {
+    public static void main(String[] args) {
+
+        int[] a = new int[4];
+        Scanner myScanner = new Scanner(System.in);
+        int[] b = null;
+
+//        System.out.println(b.length); // this won't compile since b is null (above)
+
+        String stringA = new String("A");
+        String stringB = new String("B");
+        String stringC = "hello";
+        double d; // primitive
+        Double dd; // wrapper
+
+    }
+}
