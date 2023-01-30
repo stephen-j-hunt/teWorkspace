ddbf871e5e6cdd36c45b9539e8f7223f2ab5249e
mike levy <michael.levy@techelevator.com>
2023-01-30 12:20:14 -0500

    M1D11 - final lecture files

---
+++ module-1/11_Inheritance/lecture-final/src/main/java/com/access/NoAccess.java
@@ -0,0 +1,11 @@
+package com.access;
+
+import com.techelevator.Auction;
+
+public class NoAccess {
+
+    public void nothing() {
+        Auction auction = new Auction("cup");
+    }
+
+}
---
+++ module-1/11_Inheritance/lecture-final/src/main/java/com/access/SilentAuction.java
@@ -0,0 +1,9 @@
+package com.access;
+
+import com.techelevator.Auction;
+
+public class SilentAuction extends Auction {
+    public SilentAuction(String item) {
+        super(item);
+    }
+}
