7188cf66b4c59c581889a3989ad3b8fa83a0060e
mike levy <michael.levy@techelevator.com>
2023-03-19 15:21:41 -0400

    M3D6 - lecture & exercise files

---
+++ module-3/06_Intro_to_JavaScript/lecture/script.js
@@ -0,0 +1,47 @@
+/*
+    Example of a multi-line comment just like in C#/Java
+*/
+
+// Single line comment
+
+/**
+ * Functions start with the word function.
+ * They don't have a return type and the naming convention is camel-case.
+ */
+function variables() {
+  // Declares a variable where the value cannot be changed
+  // Declares a variable those value can be changed
+  // Declares a variable that will always be an array
+}
+
+
+function doingMath() {
+
+}
+
+function stringFormatting() {
+
+}
+
+
+
+function equality() {
+
+}
+
+function objects() {
+
+}
+
+function someMathFunctions() {
+
+}
+
+function someStringFunctions() {
+
+}
+
+function workingWithArrays() {
+
+}
+
