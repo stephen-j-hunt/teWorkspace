08cfa8815a1d2c14e1fadfd4dfbb83b90bd7443a
mike levy <michael.levy@techelevator.com>
2023-03-21 11:02:57 -0400

    M3D7 - final lecture files

---
+++ module-3/07_JavaScript_Functions/lecture-final/jsdoc.js
@@ -0,0 +1,18 @@
+
+// what it does, parameters returns
+
+/**
+ * add two numbers together and return the result
+ * @param {number} p1 - the first parameter
+ * @param {number} p2 - the second parameter
+ * @returns {number} the sum of the two supplied numbers
+ */
+function add(p1, p2) {
+    return p1 + p2;
+}
+
+console.log(add(1, 2));
+
+
+
+
