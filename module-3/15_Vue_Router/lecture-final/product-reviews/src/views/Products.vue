978d7838b227c8f57247ad775dba1ee347ba6c7a
mike levy <michael.levy@techelevator.com>
2023-04-04 11:39:05 -0400

    M3D15 - final lecture files

---
+++ module-3/15_Vue_Router/lecture-final/product-reviews/src/views/Products.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="products">
+    <h1>Products</h1>
+    <!-- step 3 -->
+    <product-list />
+  </div>
+</template>
+
+<script>
+import ProductList from "../components/ProductsList.vue"; // step 1
+
+export default {
+  name: "products-view",
+  components: {
+    ProductList, // step 2
+  },
+};
+</script>
+
+<style scoped>
+</style>
